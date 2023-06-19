8323ad308e2eaba88f23f3352c982eb09d89672d
490900 <kiprotichdondl85@gmail.com>
2023-06-19 12:34:26 -0700

    ccc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,12 @@
  * main - Entry point
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {            
 	char c;
-
 	c = 'a';
 	while 
-		(c <= 'z') 
-		{
+		(c <= 'z') {
 			putchar (c);
 			c++;
 		}
