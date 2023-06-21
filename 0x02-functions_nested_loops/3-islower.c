c2a8e57277a9a4cb9fa8d8cc04dda1ad3461dd43
490900 <kiprotichdondl85@gmail.com>
2023-06-20 18:47:58 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _is slower - checks for lowercase character
+ * _islower - checks for lowercase character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ *
+ * Return: 1 if @c is a lowercase character, 0 otherwise
  */
 int _islower(int c)
-
 {
-	if (c >= 97 &&c <= 122 )
-	{
-
-	return (1);
-	}
-	return (0);
+        if (c >= 97 && c <= 122)
+        {
+                return (1);
+        }
+        return (0);
 }
+
